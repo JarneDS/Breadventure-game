ff0eaf0243a6f2c7d9209aa1510889818a4004f8
Foxx05 <theocolpaert8@gmail.com>
2025-10-14 14:50:55 +0200

    opacity removed

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -363,7 +363,6 @@ class MainWorld extends Phaser.Scene {
                     overlayBoue = this.add.image(0, 0, "boue_vue").setOrigin(0, 0);
                     overlayBoue.displayWidth  = this.sys.game.config.width;
                     overlayBoue.displayHeight = this.sys.game.config.height;
-                    overlayBoue.setAlpha(0.9);
                     overlayBoue.setScrollFactor(0);
                     overlayBoue.setDepth(999);
                     overlayStack.push(overlayBoue);
