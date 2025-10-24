4bed4dd348e5aa8e234875ba1dd2391823fde351
Foxx05 <theocolpaert8@gmail.com>
2025-10-24 10:44:13 +0200

    Modif texte intro (! pas fini)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -377,7 +377,7 @@ class LoadingScene extends Phaser.Scene {
 
         this.anims.create({
             key: 'money',
-            frames: this.anims.generateFrameNumbers('money', { start: 0, end: 11 }),
+            frames: this.anims.generateFrameNumbers('money', { start: 0, end: 10 }),
             frameRate: 11,
             repeat: -1
         })
@@ -488,7 +488,7 @@ class ExplenationScene extends Phaser.Scene {
 
         this.bg = this.add.tileSprite(0, 0, 1194, 834, 'intro').setOrigin(0, 0);
 
-        const titre = this.add.text(597, 180, 'Avis aux joueurs : ', {
+        const titre = this.add.text(597, 170, 'Avis aux joueurs : ', {
             fontSize: '36px',
             fill: '#ff0000',
             fontFamily: 'Fira Sans Condensed',
@@ -499,24 +499,21 @@ class ExplenationScene extends Phaser.Scene {
 
         titre.setOrigin(0.5, 0.5);
 
-        const txtExplicatif = this.add.text(597, 418,
-            'Découvrez le calvaire de porter des lunettes !\n' +
-            'Dans ce jeu, déplacez-vous avec les flèches latérales\n' +
+        const txtExplicatif = this.add.text(597, 425,
+            'Déplacez-vous avec les flèches latérales\n' +
             'et sautez avec la flèche du haut.\n' +
-            'Votre mission : récupérer le pain pour le dîner.\n' +
-            'Évitez flaques, pluie et le pigeon.\n' +
-            'Passez au shop pour acheter des mouchoirs (2 €)\n' +
-            'ou un parapluie (4 €) — ils vous seront précieux !\n' +
-            'Mais gardez assez d\'argent pour le pain (5 €).\n' +
+            'Dans la boutique: mouchoirs - 2$ et\n' +
+            'parapluie - 4$\n' +
+            'Dans la boulangerie: pain - 5$\n' +
             'Bonne chance !',
             {
-                fontSize: '28px',
+                fontSize: '24px',
                 fill: '#000F05',
                 fontFamily: 'Fira Sans Condensed',
                 fontStyle: 'bold',
                 backgroundColor: "rgba(255,255,255,0.4)",
-                padding: { x: 6, y: 3 },
-                lineSpacing: 10
+                padding: { x: 80, y: 77 },
+                lineSpacing: 12
             }
         );
 
